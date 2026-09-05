1cae844e6c3510073298d67ed2ee1925687c6e0f
yahay9356-netizen <yahay9356@gmail.com>
2026-09-06 01:45:46 +0300

    Start app

---
+++ android/app/src/main/kotlin/com/example/enntly/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.enntly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Eventlyapp());
+}
+class Eventlyapp extends StatelessWidget {
+  const Eventlyapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
